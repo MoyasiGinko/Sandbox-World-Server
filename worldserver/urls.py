9629ad5e52bf16f1b89cc51c10f99487feff1c04
moyasiginko <mrsiyam61@gmail.com>
2026-03-28 02:55:37 +0600

    Update urls.py

--- worldserver/urls.py
+++ worldserver/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
 urlpatterns = [
@@ -24,3 +25,5 @@ urlpatterns = [
     path('api/', include('worlds.urls')),
     path('worlds/', include('worlds.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
