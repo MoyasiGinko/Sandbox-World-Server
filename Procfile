7e5488b4243f40401a9659c46760b2ac65695cd7
moyasiginko <mrsiyam61@gmail.com>
2026-03-28 03:02:37 +0600

    modifications of deployment build commands

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn worldserver.wsgi:application --bind 0.0.0.0:$PORT
-release: python manage.py migrate
+web: sh -c "python manage.py migrate --noinput && gunicorn worldserver.wsgi:application --bind 0.0.0.0:$PORT"
+release: python manage.py collectstatic --noinput && python manage.py migrate
--- worldserver/settings.py
+++ worldserver/settings.py
@@ -146,6 +146,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -247,6 +248,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
